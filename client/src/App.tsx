80678a142c875ff2b5d7848c408858eb0f313cac
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-12-22 22:02:05 +0600

    swap language shared

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css'
+import {useState} from "react";
+import {LanguageContext} from "./context/LanguageContext";
 import Tab from "./shared/Tab/Tab";
+import LanguageSwap from "./shared/LanguageSwap/LanguageSwap";
+
 
 function App() {
 
+  const [language, setLanguage] = useState<'en'|'ru'>('en');
+
+  const changeLanguage = (lng) => {
+    if (lng === 'en' || lng === 'ru') {
+      setLanguage(lng);
+    }
+  }
+
   return (
+    <LanguageContext.Provider value={language}>
       <Tab text={'apple'} onClose={() => {console.log('clicked')}} />
+      <div style={{heigth: '100%', padding: '20px 20px', backgroundColor: '#333'}}>
+        <LanguageSwap onSwap={changeLanguage}/>
+      </div>
+
+    </LanguageContext.Provider>
+
   )
 }
 
