66f5c32cfeb821c18247511c34779e47056807b5
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-12-24 23:57:09 +0600

    add menu links + footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 import {LanguageContext} from "./context/LanguageContext";
 import Tab from "./shared/Tab/Tab";
 import LanguageSwap from "./shared/LanguageSwap/LanguageSwap";
+import Menu from "./entities/Menu/Menu";
 
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
       <div style={{heigth: '100%', padding: '20px 20px', backgroundColor: '#333'}}>
         <LanguageSwap onSwap={changeLanguage}/>
       </div>
-
+      <Menu />
     </LanguageContext.Provider>
 
   )
