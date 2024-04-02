5d1ff319b127880790d766fcca448d946ee2445e
Eshenok <102781985+Eshenok@users.noreply.github.com>
2024-04-02 11:26:31 +0300

    commit

--- client/src/pages/Sign/Sign.tsx
+++ client/src/pages/Sign/Sign.tsx
@@ -1,17 +1,16 @@
-import React, {FC, useContext} from 'react';
+import {FC, useContext} from 'react';
 import './Sign.scss';
 import Login from "../../widgets/Form/Login/Login";
 import Registration from "../../widgets/Form/Registration/Registration";
 import Title from "../../shared/Title/Title";
 import {LanguageContext} from "../../context/LanguageContext";
 import {TEXTS} from "../../constants";
-import { Redirect, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Sign: FC = () => {
 
   const context = useContext(LanguageContext);
   const location = useLocation();
-  const login = false;
 
   return (
     <section className={"auth"}>
